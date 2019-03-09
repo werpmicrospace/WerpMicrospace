198d5bcea747827544399d136038629f5755f20c
werpmicrospace <48325702+werpmicrospace@users.noreply.github.com>
2019-03-09 21:16:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -218,6 +218,6 @@ function isLoggedIn(req, res, next) {
   res.redirect("/login");
 }
 
-app.listen(3000, function () {
+app.listen(process.env.port||3000, function () {
   console.log("App is running");
 });
