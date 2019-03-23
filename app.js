930c8ad819e5adafa7c156b2bdbdf16d69350648
werpmicrospace <48325702+werpmicrospace@users.noreply.github.com>
2019-03-23 12:33:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var AdminInfo = require("./models/admin_info");
 var AdminTask=require('./models/admin_task');
 var admin=require('./routes/admin');
 
- mongoose.connect("mongodb+srv://werp:976jQJCeP4bU4ub2@werpindia-9qwtj.mongodb.net/test?retryWrites=true", {
+ mongoose.connect("mongodb+srv://werp:976jQJCeP4bU4ub2@werpindia-9qwtj.mongodb.net/microspace?retryWrites=true", {
    //mongoose.connect("mongodb://localhost:/werp_v1", {
   useNewUrlParser: true
 });
